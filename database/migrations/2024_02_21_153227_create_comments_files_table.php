5d3fc6ca17e0fce3d0b0b44a1a6ec79ef95e4530
Gleb Mizghulin <glebmiz14@gmail.com>
2024-02-21 18:34:32 +0200

    migrations

--- database/migrations/2024_02_21_153227_create_comments_files_table.php
+++ database/migrations/2024_02_21_153227_create_comments_files_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments_files', function (Blueprint $table) {
             $table->id();
+            $table->string('path');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('type');
+            $table->unsignedBigInteger('comment_id');
             $table->timestamps();
+
+            $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments_files');
     }
 };
